b76f022e74634ee495f1324d02e89238999934b0
webermuros1 <webermuros1@gmail.com>
2026-02-24 13:14:50 +0100

    Create new class Ranger

---
+++ src/main/java/org/example/Ranger.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Ranger extends Human {
+    private int trackingSkill;
+
+    public Ranger(String name, int age, String weapon, int health,
+                  String kingdom, int trackingSkill) {
+        super(name, age, weapon, health, kingdom);
+        this.trackingSkill = trackingSkill;
+    }
+
+    public int getTrackingSkill() {
+        return trackingSkill;
+    }
+    public void setTrackingSkill(int trackingSkill) {
+        this.trackingSkill = trackingSkill;
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println("Human ranger: " + getName() +
+                ", kingdom: " + getKingdom() +
+                ", tracking: " + trackingSkill +
+                ", weapon: " + getWeapon() +
+                ", health: " + getHealth());
+    }
+}
