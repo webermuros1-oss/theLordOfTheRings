a5b308e8bc4e52451e1085db40822f0dc105c8d0
webermuros1 <webermuros1@gmail.com>
2026-02-24 13:28:41 +0100

    Create class elf and Ranger

---
+++ src/main/java/org/example/Elf.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Elf extends Character {
+    private String magicType;
+
+    public Elf(String name, int age, String weapon, int health, String magicType) {
+        super(name, age, weapon, health);
+        this.magicType = magicType;
+    }
+
+    public String getMagicType() {
+        return magicType;
+    }
+    public void setMagicType(String magicType) {
+        this.magicType = magicType;
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println("Elf: " + getName() +
+                ", magic: " + magicType +
+                ", weapon: " + getWeapon() +
+                ", health: " + getHealth());
+    }
+}
