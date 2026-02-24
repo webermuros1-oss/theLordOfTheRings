8b72d54f3719613c7c501555e68e863b7ba86d4e
webermuros1 <webermuros1@gmail.com>
2026-02-24 14:17:55 +0100

    Create class Efl and HighElf

---
+++ src/main/java/org/example/HighElf.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class HighElf extends Elf {
+    private int wisdom;
+
+    public HighElf(String name, int age, String weapon, int health,
+                   String magicType, int wisdom) {
+        super(name, age, weapon, health, magicType);
+        this.wisdom = wisdom;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println("High elf: " + getName() +
+                ", magic: " + getMagicType() +
+                ", wisdom: " + wisdom +
+                ", weapon: " + getWeapon() +
+                ", health: " + getHealth());
+    }
+}
